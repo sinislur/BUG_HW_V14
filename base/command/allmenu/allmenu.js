ef64f82f3b5b2b320216d7f5e533e9b86e03b601
kintolgede60 <kintolgede60@gmail.com>
2023-03-06 12:18:51 +0700

    tes

---
+++ base/command/allmenu/allmenu.js
@@ -0,0 +1,79 @@
+const allmenu =  ` 
+╾•*Ⱡł₴₮ ₥Ɇ₦Ʉ*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏listadmin
+┣❏listban
+┣❏listjualan
+┣❏listprem
+╰━━━━━━━━━━━━╯
+
+╾•*Ø₦ⱠɎ ₲ⱤØɄ₱*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏antilink on / off
+┣❏welcome on / off
+┣❏kick 628xxx
+┣❏add 628xxx
+┣❏promote 628xx
+┣❏demote 628xx
+┣❏inspect
+┣❏linkgroup
+┣❏resetlinkgc
+┣❏tagall
+┣❏hidetag
+┣❏group
+┣❏setname
+┣❏setdesc
+┣❏editinfo
+┣❏setppgroup
+╰━━━━━━━━━━━━╯
+
+╾•*Ø₦ⱠɎ Ø₩₦ɆⱤ*•╾
+╭━━━━━━━━━━━━╮
+┣❏setppbot
+┣❏unblock
+┣❏setadmin
+┣❏setcmd
+┣❏delcmd
+┣❏nsfw
+╰━━━━━━━━━━━━╯
+
+╾•*Ɽ₳₦ĐØ₥*•╾
+╭━━━━━━━━━━━━╮
+┣❏fbvideo
+┣❏igvideo 
+┣❏twitvideo
+┣❏textmaker
+┣❏tiktokvideo
+┣❏tiktokaudio
+┣❏play
+┣❏kalkulator
+┣❏mediafire
+┣❏toimage
+┣❏toonce
+┣❏tomp4
+┣❏tovn
+┣❏togif
+┣❏tourl 
+┣❏toaud
+┣❏sticker
+┣❏stickergif
+┣❏emojimix
+┣❏emojimix2
+┣❏smeme
+┣❏pinterest
+┣❏couple
+┣❏coffe
+┣❏ktpmaker
+┣❏afk
+┣❏owner
+┣❏getname
+┣❏getpic
+┣❏infochat
+┣❏q
+┣❏del
+┣❏style
+┣❏ss
+┣❏penjara
+╰━━━━━━━━━━━━╯
+`
+exports.allmenu = allmenu
