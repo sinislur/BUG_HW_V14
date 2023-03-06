75f8e458d72f4378c7f52ec17d7f0ed33eabbff9
kintolgede60 <kintolgede60@gmail.com>
2023-03-06 13:23:42 +0700

    work

---
+++ base/command/bugmenu/bugmenu.js
@@ -0,0 +1,169 @@
+const bugmenu =  ` 
+╾•*฿Ʉ₲ Ɇ₥ØJł*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏ 🔧 [ jumlah ]
+┣❏ 🪞 [ jumlah ]
+┣❏ 🛡️ [ jumlah ]
+┣❏ 🗡️ [ jumlah ]
+┣❏ ⚔️ [ jumlah ]
+┣❏ 🦖 [ jumlah ]
+┣❏ 🦕 [ jumlah ]
+┣❏ 👿 [ jumlah ]
+┣❏ ⚡ [ jumlah ]
+┣❏ 😈 [ jumlah ]
+┣❏ ⚔️ [ jumlah ]
+┣❏ 💥 [ jumlah ]
+┣❏ 🍂 [ jumlah ]
+┣❏ 🌪️ [ jumlah ]
+┣❏ 🔥 [ jumlah ]
+┣❏ 🗿 [ jumlah ]
+┣❏ 🐕 [ jumlah ]
+┣❏ 🤣 [ jumlah ]
+┣❏ 😭 [ jumlah ]
+┣❏ 😂 [ jumlah ]
+┣❏ 🥵 [ jumlah ]
+┣❏ 🥶 [ jumlah ]
+┣❏ 😱 [ jumlah ]
+┣❏ 😎 [ jumlah ]
+┣❏button [ jumlah ]
+╰━━━━━━━━━━━━╯
+[ contoh 👿 5 ]
+
+
+╾•*฿Ʉ₲ ₲₳฿Ʉ₮*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏ kal
+┣❏ apa
+┣❏ kenapa
+┣❏ oke
+┣❏ bug
+╰━━━━━━━━━━━━╯
+[ contoh : kal ]
+
+
+╾•*VłⱤ₮ɆӾ₮ ĐɆⱠ₳Ɏ*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏ Virtext [ jumlah ]
+┣❏ Virtext2 [ jumlah ]
+┣❏ Virtext3 [ jumlah ]
+┣❏ Virtext4 [ jumlah ]
+┣❏ Virtext5 [ jumlah ]
+┣❏ Virtext6 [ jumlah ]
+┣❏ Virtext7 [ jumlah ]
+┣❏ Virtext8 [ jumlah ]
+┣❏ Virtext9 [ jumlah ]
+┣❏ Virtext10 [ jumlah ]
+╰━━━━━━━━━━━━╯
+[ Contoh Virtext1 5 ]
+
+
+╾•*₴Ɇ₦Đ ฿Ʉ₲ ₮ⱤØⱠł*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏ Sendtrol 628xxxx
+┣❏ Sendtrol2 628xxx
+┣❏ Sendtrol3 628xxx
+┣❏ Sendtrol4 628xxx
+┣❏ Sendtrol5 628xxx
+╰━━━━━━━━━━━━╯
+[ contoh Sendtrol 6285764642525 ]
+
+
+╾•*₴Ɇ₦Đ ฿Ʉ₲ ₴₳₦₮Ɇ₮*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏ Santet 628xxxx
+┣❏ Santet2 628xxx
+┣❏ Santet3 628xxx
+┣❏ Santet4 628xxx
+┣❏ Santet5 628xxx
+╰━━━━━━━━━━━━╯
+[ contoh Santet 6285764642525 ]
+
+
+╾•*₴Ɇ₦Đ ฿Ʉ₲ ĐØ₭Ʉ₥Ɇ₦*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏ Senddocu 628xxxx
+┣❏ Senddocu2 628xxx
+┣❏ Senddocu3 628xxx
+┣❏ Senddocu4 628xxx
+┣❏ Senddocu5 628xxx
+╰━━━━━━━━━━━━╯
+[ contoh Senddocu 6285764642525 ]
+
+
+╾•*₴Ɇ₦Đ ฿Ʉ₲ ⱠØ₭₳₴ł*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏ Sendlokas 628xxxx
+┣❏ Sendlokas2 628xxx
+┣❏ Sendlokas3 628xxx
+┣❏ Sendlokas4 628xxx
+┣❏ Sendlokas5 628xxx
+╰━━━━━━━━━━━━╯
+[ contoh Sendlokas 6285764642525 ]
+
+
+╾•*₴Ɇ₦Đ ฿Ʉ₲ ł₦Vł₮Ɇ*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏ Sendinvite 628xxxx
+┣❏ Sendinvite2 628xxx
+┣❏ Sendinvite3 628xxx
+┣❏ Sendinvite4 628xxx
+┣❏ Sendinvite5 628xxx
+╰━━━━━━━━━━━━╯
+[ contoh Sendinvite 6285764642525 ]
+
+
+╾•*₴Ɇ₦Đ ฿Ʉ₲ Ⱡł₴₮*•╾
+╭━━━━━━━━━━━━╮
+┣❏ Sendbuglist 628xxxx
+┣❏ Sendbuglist2 628xxx
+┣❏ Sendbuglist3 628xxx
+┣❏ Sendbuglist4 628xxx
+┣❏ Sendbuglist5 628xxx
+╰━━━━━━━━━━━━╯
+[ contoh Sendbuglist 6285764642525 ]
+
+
+╾•*₴Ɇ₦Đ VłⱤɄ₴ ł₱ / ₳₦ĐⱤØ*•╾
+╭━━━━━━━━━━━━╮
+┣❏ Sendvirus 628xxxx
+┣❏ Sendvirus2 628xxx
+┣❏ Sendvirus3 628xxx
+┣❏ Sendvirus4 628xxx
+┣❏ Sendvirus5 628xxx
+┣❏ Sendvirus6 628xxx
+┣❏ Sendvirus7 628xxx
+┣❏ Sendvirus8 628xxx
+┣❏ Sendvirus9 628xxx
+┣❏ Sendvirus10 628xx
+╰━━━━━━━━━━━━╯
+[ contoh Sendvirus 6285764642525 ]
+
+
+╾•*J₳Đł ฿Ʉ₲*•╾ 
+╭━━━━━━━━━━━━╮
+┣❏ Jadikatalog [reply sticker]
+┣❏ Jadijago [ Text nya ]
+┣❏ Jadipolling [ Text nya ]
+┣❏ Jaditroli [ Text nya ]
+┣❏ Jadilokas [ Text nya ]
+┣❏ Jadidarknes [ Text nya ]
+┣❏ Jadidocu [ Text nya ]
+┣❏ Jadibuginvite [ Text nya ]
+┣❏ Jadibugpayment [ Text nya ]
+┣❏ Jadibugsw [ Text nya ]
+┣❏ Jadibugbutton [ Text nya ]
+┣❏ Jadivirtext1 [ Text nya ]
+┣❏ Jadivirtext2 [ Text nya ]
+┣❏ Jadivirtext3 [ Text nya ]
+┣❏ Jadivirtext4 [ Text nya ]
+┣❏ Jadivirtext5 [ Text nya ]
+┣❏ Jadivirtext6 [ Text nya ]
+┣❏ Jadivirtext7 [ Text nya ]
+┣❏ Jadivirtext8 [ Text nya ]
+┣❏ Jadivirtext9 [ Text nya ]
+┣❏ Jadivirtext10 [ Text nya ]
+┣❏ buggamvn
+┣❏ buggastick
+╰━━━━━━━━━━━━╯
+[ Contoh Jadijago kontol ]`
+exports.bugmenu = bugmenu
